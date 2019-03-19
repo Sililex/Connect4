e59fbd30b3315af53bde710c1a4dd2b0b809dce9
TNiven <tom.j.niven@gmail.com>
2019-03-19 17:36:01 +1100

    Finished basic game
    Game is now functional to play with two people. MVP achieved.

--- src/Board.java
+++ src/Board.java
@@ -1,57 +1,118 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Board {
 	private String[][] board;
-	
+
 	public Board(){
 		board = new String[6][7];
 	}
 
-	public void setPiece(String piece,int x,int y){
-		board[x][y] = piece;
+	public void insertPiece(String piece, int x) {
+		int y = findTopEmptyRow(x);
+		setPiece(piece, x, y);
 	}
 
-	public static void insertPiece(String piece, int x) {
-
-
-	}
-
-	public boolean checkVictory() {
-
-
-		public boolean checkVictory(int x){
-			int y = 0;
-			int[][] dir = {{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1},{0,-1}};
-			String piece;
-
-			while (board[x][5-y]==null && y>0){
-				y--;
-			}
-			piece = board[x][y];
-
-			for(int i=0;i<dir.length;i++){
-				int xMax = x + dir[i][0]*3;
-				int yMax = y + dir[i][1]*3;
-
-				if(yMax>=0 && xMax>=0 && yMax<6 && xMax < 7){
-
-				}
-			}
-
-
-
-			return false;
+	public boolean checkVictory(int mostRecentColumn) {
+		boolean victory = false;
+		int mostRecentRow;
+		try {
+			mostRecentRow = findTopEmptyRow(mostRecentColumn) - 1;
 		}
+		catch(Exception e){
+			mostRecentRow = 5;
+		}
+		// Define possible paths
+		boolean canLeft = true;
+		boolean canUp = true;
+		boolean canRight = true;
+		boolean canDown = true;
+		boolean canUpRight = true;
+		boolean canUpLeft = true;
+		boolean canDownRight = true;
+		boolean canDownLeft = true;
+
+		// Check possible paths
+		if(mostRecentColumn < 3) {
+			canDownLeft = false;
+			canUpLeft = false;
+			canLeft = false;
+		}
+		if(mostRecentColumn > 3) {
+			canDownRight = false;
+			canUpRight = false;
+			canRight = false;
+		}
+		if(mostRecentRow < 3) {
+			canDownLeft = false;
+			canDownRight = false;
+			canDown = false;
+		}
+		if(mostRecentRow > 2) {
+			canUpRight = false;
+			canUpLeft = false;
+			canUp = false;
+		}
+
+		// Define paths
+		int x = mostRecentColumn;
+		int y = mostRecentRow;
+		int[][] leftPath = new int[][]{{x,y}, {x-1,y}, {x-2,y}, {x-3,y}};
+		int[][] upPath = new int[][]{{x,y}, {x,y+1}, {x,y+2}, {x,y+3}};
+		int[][] rightPath = new int[][]{{x,y}, {x+1,y}, {x+2,y}, {x+3,y}};
+		int[][] downPath = new int[][]{{x,y}, {x,y-1}, {x,y-2}, {x,y-3}};
+		int[][] upRightPath = new int[][]{{x,y}, {x+1,y+1}, {x+2,y+2}, {x+3,y+3}};
+		int[][] upLeftPath = new int[][]{{x,y}, {x-1,y+1}, {x-2,y+2}, {x-3,y+3}};
+		int[][] downRightPath = new int[][]{{x,y}, {x+1,y-1}, {x+2,y-2}, {x+3,y-3}};
+		int[][] downLeftPath = new int[][]{{x,y}, {x-1,y-1}, {x-2,y-2}, {x-3,y-3}};
+
+		// Apply possible path tests
+		if(canLeft && checkPathSame(leftPath)){
+			victory = true;
+		}
+		if(canUp && checkPathSame(upPath)){
+			victory = true;
+		}
+		if(canRight && checkPathSame(rightPath)){
+			victory = true;
+		}
+		if(canDown && checkPathSame(downPath)){
+			victory = true;
+		}
+		if(canUpRight && checkPathSame(upRightPath)){
+			victory = true;
+		}
+		if(canUpLeft && checkPathSame(upLeftPath)){
+			victory = true;
+		}
+		if(canDownRight && checkPathSame(downRightPath)){
+			victory = true;
+		}
+		if(canDownLeft && checkPathSame(downLeftPath)){
+			victory = true;
+		}
+
+		return victory;
+	}
+
+	public boolean checkEmptyTop(int x) {
+		return board[5][x] == null;
 	}
 
 	public void printBoard() {
 		for(int y = 5; y >= 0; y = y-1){
 			String line = "| ";
 			for(int x = 0; x < 6; x++) {
-					if(this.board[y][x] == null) {
-						line = line + "  | ";
-					}
-					else {
-						line = line + this.board[y][x] + " | ";
-					}
+				if(this.board[y][x] == null) {
+					line = line + "  | ";
+				}
+				else {
+					line = line + this.board[y][x] + " | ";
+				}
 			}
 			if(this.board[y][6] == null) {
 				line = line + "  |";
@@ -61,5 +122,39 @@ public class Board {
 			}
 			System.out.println(line);
 		}
+		System.out.println("  1   2   3   4   5   6   7   ");
+	}
+
+	private void setPiece(String piece, int x, int y){
+		board[y][x] = piece;
+	}
+
+	private int findTopEmptyRow(int x) {
+		int topEmpty = 0;
+		for(int y = 0; y < 6; y++) {
+			if(board[y][x] != null) {
+				if(y != 5) {
+					topEmpty = y + 1;
+				}
+				else {
+					throw new IndexOutOfBoundsException("No empty rows at column " + Integer.toString(x));
+				}
+			}
+			else {
+				return topEmpty;
+			}
+		}
+		return 0;
+	}
+
+	private boolean checkPathSame(int[][] path) {
+		String symbol = board[path[0][1]][path[0][0]];
+		boolean same = true;
+		for(int i = 0; i < path.length; i++){
+			if(symbol != board[path[i][1]][path[i][0]]){
+				same = false;
+			}
+		}
+		return same;
 	}
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class main {
     private static Board board = new Board();
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.print("Hello! Welcome to connect four! Please alternate turns between players.\n" +
                 "The aim of the game is to connect four in a row on the board either diagonally,\n" +
@@ -11,24 +15,45 @@ public class main {
     }
 
     public static void ticker(int turn) {
-        String[] turnStrings = {"x","o"};
-        String turnString = turnStrings[turn%2];
+        String[] turnStrings = {"x", "o"};
+        String turnString = turnStrings[turn % 2];
 
         System.out.println("\n\n\nTurn: " + Integer.toString(turn));
         board.printBoard();
-        System.out.print("It is " + turnString + "'s turn. Press the number you would like to go in.");
-        int location = Integer.parseInt(System.console().readLine());
+        System.out.print("It is " + turnString + "'s turn. Press the number you would like to go in: ");
+        String input = getColumnInput();
+        int location = Integer.parseInt(input) - 1;
         board.insertPiece(turnString, location);
-        if(board.checkVictory()){
+        if (board.checkVictory(location)) {
+            board.printBoard();
             victory(turnString);
-        }
-        else{
+        } else {
             ticker(turn + 1);
         }
     }
 
     private static void victory(String victor) {
         System.out.print("Congratulations " + victor + "! You win!");
-        System.exit(1);
+        System.exit(0);
     }
-};
+
+    private static String getColumnInput() {
+        boolean valid = false;
+        String input = new String();
+        while (!valid) {
+            input = scanner.nextLine();
+            try {
+                int number = Integer.parseInt(input);
+                if(number < 8 && number > 0 && board.checkEmptyTop(number-1)) {
+                    valid = true;
+                }
+                else{
+                    System.out.print("Please put a number between 1-7 without any spaces or other letters\nPress the number you would like to go in: ");
+                }
+            } catch (Exception e) {
+                System.out.print("Please put a number between 1-7 without any spaces or other letters:\nPress the number you would like to go in: ");
+            }
+        }
+        return input;
+    }
+}
