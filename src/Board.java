4d5740f9b8990a9111fbed5878dbcbe94436bce6
TNiven <tom.j.niven@gmail.com>
2019-03-19 15:41:32 +1100

    Added printBoard method
    Fixed misc bugs

--- src/Board.java
+++ src/Board.java
@@ -2,8 +2,40 @@ public class Board {
 	private String[][] board;
 	
 	public Board(){
-		this.board = new String[6][7];
+		board = new String[6][7];
 	}
 
-	
+	public void setPiece(String piece, int x, int y){
+
+	}
+
+	public static void insertPiece(String piece, int x) {
+
+
+	}
+
+	public static boolean checkVictory() {
+		return false;
+	}
+
+	public void printBoard() {
+		for(int y = 5; y >= 0; y = y-1){
+			String line = "| ";
+			for(int x = 0; x < 6; x++) {
+					if(this.board[y][x] == null) {
+						line = line + "  | ";
+					}
+					else {
+						line = line + this.board[y][x] + " | ";
+					}
+			}
+			if(this.board[y][6] == null) {
+				line = line + "  |";
+			}
+			else {
+				line = line + this.board[y][6] + " |";
+			}
+			System.out.println(line);
+		}
+	}
 }
--- src/main.java
+++ src/main.java
@@ -1,10 +1,12 @@
 public class main {
+    private static Board board = new Board();
     public static void main(String[] args) {
         System.out.print("Hello! Welcome to connect four! Please alternate turns between players.\n" +
                 "The aim of the game is to connect four in a row on the board either diagonally,\n" +
                 "horizontally or vertically. Press the corresponding number to insert your piece.\n" +
                 "\n" +
                 "Have fun!");
+
         ticker(1);
     }
 
@@ -12,7 +14,7 @@ public class main {
         String[] turnStrings = {"x","o"};
         String turnString = turnStrings[turn%2];
 
-        System.out.println("Turn: " + Integer.toString(turn));
+        System.out.println("\n\n\nTurn: " + Integer.toString(turn));
         board.printBoard();
         System.out.print("It is " + turnString + "'s turn. Press the number you would like to go in.");
         int location = Integer.parseInt(System.console().readLine());
