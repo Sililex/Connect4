6347b06f29da98f81400b54766bf2eed96883033
TNiven <tom.j.niven@gmail.com>
2019-03-19 14:16:39 +1100

    Added instructions
    Wrote instructional introduction paragraph.

--- src/main.java
+++ src/main.java
@@ -1,2 +1,12 @@
+
+
 public class main {
-}
+    public static void main(String[] args) {
+        System.out.print("Hello! Welcome to connect four! Please alternate turns between players.\n" +
+                "The aim of the game is to connect four in a row on the board either diagonally,\n" +
+                "horizontally or vertically. Press the corresponding number to insert your piece.\n" +
+                "\n" +
+                "Have fun!");
+
+    };
+};
